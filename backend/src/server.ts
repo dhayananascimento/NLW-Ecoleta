047fb0efca2e4685e53ec8ff60ceb68d6d5a94e6
Dhayana <contatodhayana@gmail.com>
2020-06-03 14:14:48 -0300

    aula 02 - backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,13 @@
-import express from 'express';
+import express from 'express'
+import cors from 'cors'
+import routes from './routes'
+import path from 'path'
 
-const app = express();
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
-app.get('/users',(request,response) => {
-    console.log("Listagem de usuarios");
-    response.json([
-        "Dhayana",
-        "Julia",
-        "Ana",
-        "Maria",
-        "Lucia"
-    ])
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
-app.listen(3333);
+app.listen(3333)
